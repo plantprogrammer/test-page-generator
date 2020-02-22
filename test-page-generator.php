48785164fd2e148edf2f79222d52e2d7f855dbda
Ian <ian.sajkowicz@uconn.edu>
2020-02-22 13:25:28 -0500

    Add files via upload

--- test-page-generator.php
+++ test-page-generator.php
@@ -7,7 +7,31 @@ functionality of a plug-in
 * Version: 1.0
 * Author: Ian
 * Author URI: https://iansackofwits.com
+* License: GPLv2 or later
 **/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright (C) 2020 Ian
+*/
+
+if (!defined('ABSPATH'))
+{
+	wp_die;
+}
 
 /**
  * Summary.
@@ -31,11 +55,12 @@ if (!function_exists("create_test_posts"))
 	function create_test_posts()
 	{
 		$post_data = array (
-			$name = "Trial",
-			$type = "post",
-			$content = "Test",
-			$status = "publish"
+			$name => "Trial",
+			$type => "post",
+			$content => "Test",
+			$status => "publish"
 		);
 		wp_insert_post($post_data);
 		
 	}
+}
