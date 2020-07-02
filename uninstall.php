0c77f4429b4e0792209a629c8d267a13db991f32
Ian <ian.sajkowicz@uconn.edu>
2020-07-02 14:52:29 -0400

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -12,8 +12,7 @@ if (!defined("WP_UNINSTALL_PLUGIN"))
 
 $cat_ID = get_cat_ID(CAT_NAME);
 $posts = get_posts(array("post_status" => "any, trash","post_type" => "post", "numberposts" => -1, "category" => array($cat_ID)));
-var_dump($posts);
-wp_die();
+
 foreach($posts as $post)
 {
 	wp_delete_post($post->ID,true);
