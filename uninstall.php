0af0dde914d94742cc4cc72c0752c7616b1199af
Ian <ian.sajkowicz@uconn.edu>
2020-07-03 17:40:24 -0400

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,15 +2,15 @@
 
 namespace test_page_generator;
 
-define("SETTING_NAME", "test_post_generator_num_pages");
-define("CAT_NAME", "Test Post");
+define("TEST_POST_SETTING_NAME", "test_post_generator_num_pages");
+define("TEST_POST_CAT_NAME", "Test Post");
 
 if (!defined("WP_UNINSTALL_PLUGIN"))
 {
 	wp_die();
 }
 
-$cat_ID = get_cat_ID(CAT_NAME);
+$cat_ID = get_cat_ID(TEST_POST_CAT_NAME);
 $posts = get_posts(array("post_status" => "any, trash","post_type" => "post", "numberposts" => -1, "category" => array($cat_ID)));
 
 foreach($posts as $post)
@@ -19,4 +19,6 @@ foreach($posts as $post)
 }
 
 wp_delete_category($cat_ID);
-delete_option(SETTING_NAME);
+delete_option(TEST_POST_SETTING_NAME);
+
+
